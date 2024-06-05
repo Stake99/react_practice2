0827116b895ce1a8d68357f62e45b781606465b3
Onalerona Maine <mainekgotso@gmail.com>
2024-06-05 17:33:34 +0200

    Understanding Next js and its fundamentals

---
+++ next-app/app/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react'
+import AddToCard from './AddToCard';
+import styles from './ProductCard.module.css';
+
+
+const ProductCard = () => {
+  return (
+    <div className={styles.card}>
+        <AddToCard />
+    </div>
+  )
+}
+
+export default ProductCard
